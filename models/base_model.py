ddba13dcb93482bafba813d4729f93492109e8c2
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-07-10 16:25:36 +0100

    ubasemodel (untested)

---
+++ models/base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    BaseModel - a module that defines all common attributes/methods for other classes
+"""
+
+
+import uuid
+from datetime import datetime as time
+
+
+class BaseModel:
+    """ class to define all common attributes/methods """
+
+    def __init__(self):
+        """ initialize an instance """
+        self.id = str(uuid.uuid4())
+        self.created_at = time.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """ return the string representation of an instance """
+        return "{} ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """ modifies the date of update for the instance """
+        self.updated_at = time.now()
+
+    def __dict__(self):
+        """ eturns a dictionary containing all keys/values of __dict__ of the instance: """
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        return my_dict
