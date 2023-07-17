40e0629ab617a63c2a8fdcbfd12d7566925d2ca5
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-07-17 13:21:24 +0100

    more console tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -59,3 +59,12 @@ EOF  all  count  create  destroy  help  quit  show  update
                 HBNBCommand().onecmd("create foo")
             self.assertEqual(f.getvalue().strip("\n"), "** class doesn't exist **")
     
+    def test_show(self):
+        for _class in HBNBCommand.classes.keys():
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("create " + _class)
+            _id = f.getvalue().strip("\n")
+            with patch('sys.stdout', new=StringIO()) as f:
+                HBNBCommand().onecmd("show " + _class + " " + _id)
+            self.assertTrue(_id in f.getvalue())
+            self.assertTrue(_class in f.getvalue())
